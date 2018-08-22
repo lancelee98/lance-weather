1d374cb9dc34639c082818efccf4cff933b56517
lancelee98 <911749650@qq.com>
2018-08-22 13:54:54 +0800

    add BaiduLocation sdk,and HeWeather sdk

--- app/src/main/java/com/lanceweather/android/MainActivity.java
+++ app/src/main/java/com/lanceweather/android/MainActivity.java
@@ -1,23 +1,142 @@
 package com.lanceweather.android;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import interfaces.heweather.com.interfacesmodule.bean.Lang;
+import interfaces.heweather.com.interfacesmodule.bean.Unit;
+import interfaces.heweather.com.interfacesmodule.bean.weather.now.Now;
+import interfaces.heweather.com.interfacesmodule.view.HeConfig;
+import interfaces.heweather.com.interfacesmodule.view.HeWeather;
+
+public class MainActivity extends AppCompatActivity {
+    public LocationClient mLocationClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        HeConfig.init("HE1808161509581231", "87ba45595c7b4ba78a14c61eeaae2316");
+        HeConfig.switchToFreeServerNode();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         if(preferences.getString("weather",null)!=null){
             Intent intent = new Intent(this,WeatherActivity.class);
             startActivity(intent);
             finish();
         }
+        mLocationClient=new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        List<String> permissionList=new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE)!=
+                PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!=
+                PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)!=
+                PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if(!permissionList.isEmpty()){
+            String [] permissions=permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }
+        else requestLocation();
+
+    }
+    private   void requestLocation(){
+        initLocation();
+        mLocationClient.start();
+    }
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setIsNeedAddress(true);
+        option.setScanSpan(5000);
+        mLocationClient.setLocOption(option);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case 1:
+                if(grantResults.length>0){
+                    boolean status=true;
+                    for(int result :grantResults)
+                    {
+                        if(result!=PackageManager.PERMISSION_GRANTED){
+                            status=false;
+                        }
+                    }
+                    if(status==false)
+                    {
+                        Toast.makeText(this,"未同意定位权限，请您手动选择城市。",Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.activity_main);
+                    }
+                    else if(status==true)
+                    {
+                        Toast.makeText(this,"已同意定位权限，自动帮您选择城市。",Toast.LENGTH_SHORT).show();
+                        requestLocation();
+                    }
+                }
+                else {
+                    Toast.makeText(this,"未知错误。",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(final BDLocation location){
+            String s=location.getLongitude()+","+location.getLatitude();
+            Log.i("Log", "onSuccess: "+s);
+            HeWeather.getWeatherNow(getApplicationContext(), s, Lang.CHINESE_SIMPLIFIED, Unit.METRIC,
+                    new HeWeather.OnResultWeatherNowBeanListener() {
+                        @Override
+                        public void onError(Throwable throwable) {
+                            Log.i("Log", "onError: ", throwable);
+                        }
+
+                        @Override
+                        public void onSuccess(List<Now> list) {
+                            String weatherId=list.get(0).getBasic().getCid();
+                            Intent intent = new Intent(getApplicationContext(),WeatherActivity.class);
+                            intent.putExtra("weather_id",weatherId);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+
+        }
+    }
+
+   @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
     }
 }
--- app/src/main/java/com/lanceweather/android/WeatherActivity.java
+++ app/src/main/java/com/lanceweather/android/WeatherActivity.java
@@ -2,15 +2,13 @@ package com.lanceweather.android;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -54,16 +52,6 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*//背景图层融入状态栏
-
-        if(Build.VERSION.SDK_INT>=21){
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            );
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }*/
-
         setContentView(R.layout.activity_weather);
 
         weatherLayout =(ScrollView) findViewById(R.id.weather_layout) ;
@@ -149,7 +137,7 @@ public class WeatherActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
             final String responseText =response.body().string();
-            final  Weather weather= Utility.handleWeatherResponse(responseText);
+            final Weather weather= Utility.handleWeatherResponse(responseText);
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -168,6 +156,7 @@ public class WeatherActivity extends AppCompatActivity {
             });
             }
         });
+
     }
     private void showWeatherInfo(Weather weather)
     {
